eae78de534da95c5c756585dd07bddd8137f1bb4
Michel Maia <programador.michel.maia@gmail.com>
2022-10-26 18:41:22 -0300

    commit
    criado o arquivo etapas.js

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Nananinanão',
+                partido: 'EFG',
+                fotos: [
+                    {url:'38111.jpg', legenda:'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Çocorro',
+                partido: 'XPTO',
+                fotos: [
+                    {url:'38111.jpg', legenda:'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Seu Nunca',
+                partido: 'EFG',
+                vice: 'AhNão',
+                fotos: [
+                    {url:'99.jpg', legenda:'Prefeito', small: false},
+                    {url:'99_2.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Credo',
+                partido: 'XPTO',
+                vice: 'Eca',
+                fotos: [
+                    {url:'84.jpg', legenda:'Prefeito', small: false},
+                    {url:'84_2.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+    
+];
